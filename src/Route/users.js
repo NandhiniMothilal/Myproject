55da36d6f210a9f451c02a9f449f2681067c436f
Naganandhini <naganandhininovastrid@gmail.com>
2023-06-23 12:28:25 +0530

    prettier changes

--- src/Route/users.js
+++ src/Route/users.js
@@ -1,8 +1,8 @@
-import express from 'express';
-const router = express.Router();
+import express from 'express'
+const router = express.Router()
 
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
+  res.send('respond with a resource')
+})
 
-export default router;
+export default router
