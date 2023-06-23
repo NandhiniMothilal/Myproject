337746bfd987f2bf58a60a0959c25bcdf1e341b6
Naganandhini <naganandhininovastrid@gmail.com>
2023-06-23 12:28:07 +0530

    prettier changes

--- src/Route/index.js
+++ src/Route/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-const router = express.Router();
+import express from 'express'
+const router = express.Router()
 
 router.get('/', function (req, res) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Express' })
+})
 
-export default router;
+export default router
