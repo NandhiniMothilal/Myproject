268c80533b0b08e15f7a1a18e45bb70d05eae9bc
Naganandhini <naganandhininovastrid@gmail.com>
2023-06-23 22:09:54 +0530

    response change

--- src/Route/index.js
+++ src/Route/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', function (req, res) {
-  res.render('index', { title: 'Express' })
+  res.json({ status: true, message: 'success' })
 })
 
 export default router
