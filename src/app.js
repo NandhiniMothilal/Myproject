ce75e0ed77b99281f382ae3ecc06a964dba99f41
Naganandhini <naganandhininovastrid@gmail.com>
2023-06-23 12:27:34 +0530

    prettier changes

--- src/app.js
+++ src/app.js
@@ -1,46 +1,46 @@
 // packages
-import createError from 'http-errors';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import createError from 'http-errors'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import path from 'path'
+import { fileURLToPath } from 'url'
 // Route
-import indexRouter from './Route/index.js';
-import usersRouter from './Route/users.js';
+import indexRouter from './Route/index.js'
+import usersRouter from './Route/users.js'
 // Helper
-import EnvData from './Helper/EnvData.js';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import EnvData from './Helper/EnvData.js'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const app = express();
+const app = express()
 // view engine setup
-app.set('views', path.join(__dirname, 'View'));
-app.set('view engine', 'jade');
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'View'))
+app.set('view engine', 'jade')
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.status(err.status || 500)
+  res.render('error')
+})
 
 app.listen(EnvData.port, () => {
-  console.log(`Server is running on port ${EnvData.port}`);
-});
+  console.log(`Server is running on port ${EnvData.port}`)
+})
 
-export default app;
+export default app
